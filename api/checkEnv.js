a903226c78d56e48fe430255a5e1b1aae5a1f85c
linasobhy8-source <linasobhy8@gmail.com>
2026-03-28 01:40:12 +0200

    Create checkEnv.js
    Signed-off-by: linasobhy8-source <linasobhy8@gmail.com>

---
+++ api/checkEnv.js
@@ -0,0 +1,8 @@
+// checkEnv.js
+console.log("SERPAPI_KEY:", !!process.env.SERPAPI_KEY);
+console.log("MONGODB_URI:", !!process.env.MONGODB_URI);
+console.log("GA_MEASUREMENT_ID:", !!process.env.GA_MEASUREMENT_ID);
+console.log("FB_PIXEL_ID:", !!process.env.FB_PIXEL_ID);
+console.log("AMAZON_US:", !!process.env.AMAZON_US);
+console.log("AMAZON_CA:", !!process.env.AMAZON_CA);
+console.log("AMAZON_EG:", !!process.env.AMAZON_EG);

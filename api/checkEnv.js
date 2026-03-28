b98701488953cb2199961f6e5ae245170f64e12b
linasobhy8-source <linasobhy8@gmail.com>
2026-03-28 04:48:27 +0200

    Update checkEnv.js
    Signed-off-by: linasobhy8-source <linasobhy8@gmail.com>

--- api/checkEnv.js
+++ api/checkEnv.js
@@ -1,8 +1,27 @@
-// checkEnv.js
-console.log("SERPAPI_KEY:", !!process.env.SERPAPI_KEY);
-console.log("MONGODB_URI:", !!process.env.MONGODB_URI);
-console.log("GA_MEASUREMENT_ID:", !!process.env.GA_MEASUREMENT_ID);
-console.log("FB_PIXEL_ID:", !!process.env.FB_PIXEL_ID);
-console.log("AMAZON_US:", !!process.env.AMAZON_US);
-console.log("AMAZON_CA:", !!process.env.AMAZON_CA);
-console.log("AMAZON_EG:", !!process.env.AMAZON_EG);
+// api/checkEnv.js
+
+export default function handler(req, res) {
+  try {
+    const keys = {
+      SERPAPI_KEY: !!process.env.SERPAPI_KEY,
+      MONGODB_URI: !!process.env.MONGODB_URI,
+      GA_MEASUREMENT_ID: !!process.env.GA_MEASUREMENT_ID,
+      FB_PIXEL_ID: !!process.env.FB_PIXEL_ID,
+      AMAZON_US: !!process.env.AMAZON_US,
+      AMAZON_CA: !!process.env.AMAZON_CA,
+      AMAZON_EG: !!process.env.AMAZON_EG
+    };
+
+    res.status(200).json({
+      success: true,
+      message: "Environment keys check",
+      keys
+    });
+  } catch (error) {
+    console.error("Error in /api/checkEnv:", error);
+    res.status(500).json({
+      success: false,
+      message: "Internal Server Error"
+    });
+  }
+}

94a55ee814c9c97d227d31aa4f4e7db5e0d6452e
linasobhy8-source <linasobhy8@gmail.com>
2026-03-29 20:58:46 +0200

    Update products.js
    Signed-off-by: linasobhy8-source <linasobhy8@gmail.com>

--- api/products.js
+++ api/products.js
@@ -30,10 +30,30 @@ export default function handler(req, res) {
     const products = [
       {
         title: "Echo Dot (5th Gen)",
+        price: "$49.99",
+        rating: 4.7,
+        image: "https://m.media-amazon.com/images/I/61u0y9ADElL._AC_SL1000_.jpg",
+        link: `https://${domain}/dp/B09B8Q3YHJ?tag=${tag}`
+      },
+      {
+        title: "Fire TV Stick 4K",
+        price: "$59.99",
+        rating: 4.8,
+        image: "https://m.media-amazon.com/images/I/51CgKGfMelL._AC_SL1000_.jpg",
+        link: `https://${domain}/dp/B08XVYZ1Y5?tag=${tag}`
+      },
+      {
+        title: "Kindle Paperwhite",
+        price: "$129.99",
+        rating: 4.6,
+        image: "https://m.media-amazon.com/images/I/61fPYvC5RAL._AC_SL1000_.jpg",
         link: `https://${domain}/dp/B08N36XNTT?tag=${tag}`
       },
       {
         title: "Smart Watch Pro",
+        price: "$39.99",
+        rating: 4.5,
+        image: "https://m.media-amazon.com/images/I/61IMRs+o0iL._AC_SL1500_.jpg",
         link: `https://${domain}/dp/B09V7Z4TJG?tag=${tag}`
       }
     ];
